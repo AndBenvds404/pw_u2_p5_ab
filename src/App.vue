ecc12647a0c702e017a5243bf0bf90e4deeffa71
AndBenvds404 <andres_benavides17@hotmail.com>
2023-12-21 19:48:02 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome aaaaaaaaaaaaaa to Your Vue.js App"/>
+  <HelloWorld msg="Hola mundo.js App"/>
+  <HelloWorld msg="Hola mundo.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
