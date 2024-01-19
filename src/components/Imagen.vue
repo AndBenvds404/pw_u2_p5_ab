12ac261581683102c32e16a8e4718df1b91a76a8
AndBenvds404 <andres_benavides17@hotmail.com>
2024-01-18 20:51:08 -0500

    taller24 Correccion Prueba1

---
+++ src/components/Imagen.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container">
+        
+        <img class="img-caja" :src="urlImg" alt="">
+        <label for="">{{ respuesta }}</label>
+
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    },
+
+    props:{
+        respuesta:{
+            default:'xxxxxxx'
+        },
+        urlImg:{
+            type: String
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+.container{
+    display: grid;
+   justify-content: center;
+   align-items: center;
+   margin: 0px 37px
+}
+
+
+.img-caja{
+    width: 200px;
+    height: 300px;
+}
+
+
+label{
+    font-size: 27px;
+}
+</style>
